b624e5d5ac80087142fa2c114e2b5658fd6ba879
Christopher Jr. <christopherjrcabatbat016@gmail.com>
2024-10-05 21:30:13 +0800

    admin start

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,23 +10,72 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo-icon.png') }}">
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin-styles.css') }}">
 
     <script src="https://kit.fontawesome.com/f416851b63.js" crossorigin="anonymous"></script>
 
-@yield('styles-links')
+    @yield('styles-links')
 
 </head>
+
 <body>
+
+    <header>
+        {{-- Top Nav --}}
+        <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #e3f2fd;">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('images/logo-name.png') }}" width="148" height="" alt="Pisces logo">
+                </a>
     
-    <header></header>
-
+                <div class="navbar-scroll" id="navbarScroll">
+                    <div>
+                        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                            <li class="nav-item dropdown position-relative">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    {{ Auth::user()->first_name }}
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li>
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <button class="dropdown-item" type="submit">Log out</button>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    
+        {{-- Sidebar --}}
+        <div class="sidebar">
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Menu</a></li>
+            </ul>
+        </div>
+    
+    </header>
+    
     <main>
-        @yield('main-content')  
+        @yield('main-content')
     </main>
-
-    <footer></footer>
+    
 
     @yield('scripts')
 
+    <script src="{{ asset('js/scripts.js') }}"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.js') }}"></script>
+
 </body>
+
 </html>
